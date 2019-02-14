d418b5db015ee3b56d263c09106b52d7b0f75328
Jeffrey Horner <jeffhorner@Jeffreys-MacBook-Pro.local>
2019-02-14 14:18:40 -0600

    installed more dependencies, created base MainHeader component, added MainHeader to default layout

---
+++ components/MainHeader.vue
@@ -0,0 +1,60 @@
+<template>
+    <header>
+        <nav class="primaryNavigation">
+            <ul 
+                v-for="item in nav.items"
+                v-bind:key="item.name"
+            >
+                <li class="navItem">
+                    <a v-bind:href="item.link">{{ item.name }}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                nav: {
+                    items: [
+                        {
+                            name: 'home',
+                            link: '/',
+                        },
+                        {
+                            name: 'about',
+                            link: '/about',
+                        },
+                        {
+                            name: 'portfolio',
+                            link: '/portfolio',
+                        },
+                        {
+                            name: 'learn-to-code',
+                            link: '/learn-to-code',
+                        },
+                        {
+                            name: 'contact',
+                            link: '/contact',
+                        },
+                    ],
+                },
+            };
+        }
+    }
+</script>
+
+<style lang="scss">
+    // @import '@assets/css'
+
+    .primaryNavigation {
+        @apply flex flex-row;
+
+        .navItem {
+            @apply list-reset;
+        }
+    }
+
+</style>
--- components/Test.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        this is a test <font-awesome-icon icon="coffee" />
-    </div>
-</template>
